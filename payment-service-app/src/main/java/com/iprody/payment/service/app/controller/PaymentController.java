2c2730fcc248405f824c437b080157f7ee6ccce7
Knarik8 <k_armina@mail.ru>
2025-07-23 23:41:11 +0300

    - update Docker Compose to include Postgres and pgAdmin - add Spring Data JPA dependencies in pom.xml - configure application.yml for Postgres - remove in-memory Payment entity and implement new JPA entity - create PaymentRepository (CrudRepository) - update PaymentController to use repository for GET /payments and /payments/{id}

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -1,33 +1,31 @@
 package com.iprody.payment.service.app.controller;
 
-import com.iprody.payment.service.app.model.Payment;
+import com.iprody.payment.service.app.persistence.entity.Payment;
+import com.iprody.payment.service.app.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
 
-    private final Payment payment = new Payment(1L, 99.99);
-    private final Payment payment1 = new Payment(2L, 299.99);
-    private final Payment payment2 = new Payment(3L, 399.99);
-    private final Payment payment3 = new Payment(4L, 499.99);
-    private final Map<Long, Payment> paymentStorage = Map.of(1L, payment, 2L, payment1, 3L, payment2, 4L, payment3);
+    @Autowired
+    private PaymentRepository paymentRepository;
 
-    @GetMapping
-    public List<Payment> getPayments() {
-        return new ArrayList<>(paymentStorage.values());
+    @GetMapping("/all")
+    public List<Payment> getAll() {
+        return paymentRepository.findAll();
     }
 
-    @GetMapping("/{id}")
-    public Payment getPaymentById(@PathVariable Long id) {
-        return paymentStorage.get(id);
+    @GetMapping("/{guid}")
+    public Optional<Payment> getById(@PathVariable UUID guid) {
+        return paymentRepository.findById(guid);
     }
-
 }
